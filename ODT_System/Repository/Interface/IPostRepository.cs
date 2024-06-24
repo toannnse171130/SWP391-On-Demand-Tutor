e909cfacc9b6b6d163deca6ac5b84c62939e10f5
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-24 22:08:27 +0700

    Create IPostRepository

---
+++ ODT_System/Repository/Interface/IPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using ODT_System.Models;
+
+namespace ODT_System.Repository.Interface
+{
+    public interface IPostRepository : IBaseRepository<Post>
+    {
+        public Post? FindByIdIncludeStudyTimes(int id);
+        public Post? FindByIdIncludeAll(int id);
+
+    }
+}
