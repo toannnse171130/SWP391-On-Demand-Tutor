a6b6b4f51ded39a1c0a938386729de7b5b69becb
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-24 22:09:55 +0700

    update UserRepository

--- ODT_System/Repository/UserRepository.cs
+++ ODT_System/Repository/UserRepository.cs
@@ -1,9 +1,9 @@
 ﻿using ODT_System.Models;
 using ODT_System.Repository.Interface;
-
+using Microsoft.EntityFrameworkCore;
 namespace ODT_System.Repository
 {
-    public class UserRepository : BaseRepository, IUserRepository
+    public class UserRepository : BaseRepository<User>, IUserRepository
     {
         private readonly OdtsystemContext _context;
 
@@ -32,6 +32,11 @@ namespace ODT_System.Repository
         public User? FindByEmail(string email)
         {
             return _context.Users.FirstOrDefault(u => u.Email == email);
+        }
+
+        public User? FindByEmailIncludeRole(string email)
+        {
+            return _context.Users.Include(u => u.Role).FirstOrDefault(u => u.Email == email);
         }
 
         public User? FindById(int userId)
