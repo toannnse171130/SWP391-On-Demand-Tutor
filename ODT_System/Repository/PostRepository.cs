b37c20371d3646b8261ec8f50ddb442168b4429e
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-24 22:09:27 +0700

    Create PostRepository

---
+++ ODT_System/Repository/PostRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ODT_System.Models;
+using ODT_System.Repository.Interface;
+
+namespace ODT_System.Repository
+{
+    public class PostRepository : BaseRepository<Post>, IPostRepository
+    {
+        public PostRepository(OdtsystemContext context) : base(context)
+        {
+        }
+
+        public Post? FindByIdIncludeAll(int id)
+        {
+            return _dbSet.Include(p => p.StudyTimes).Include(p => p.User).FirstOrDefault(p => p.Id == id && p.IsDeleted == false);
+        }
+
+        public Post? FindByIdIncludeStudyTimes(int id)
+        {
+            return _dbSet.Include(p => p.StudyTimes).FirstOrDefault(p => p.Id == id && p.IsDeleted == false);
+        }
+    }
+}
