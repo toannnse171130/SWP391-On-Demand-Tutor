430e9499507fe46ef1bd384c85c599ad20691b0f
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-24 22:07:33 +0700

    Create Chat Repository

---
+++ ODT_System/Repository/ChatRepository.cs
@@ -0,0 +1,13 @@
+﻿using ODT_System.Models;
+using ODT_System.Repository.Interface;
+
+namespace ODT_System.Repository
+{
+    public class ChatRepository : BaseRepository<Chat>, IChatRepository
+    {
+        public ChatRepository(OdtsystemContext context) : base(context)
+        {
+
+        }
+    }
+}
