8b69fd32d7476a228739638284c158a63d3c8169
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-24 22:09:13 +0700

    update UserRepository

--- ODT_System/Repository/Interface/IUserRepository.cs
+++ ODT_System/Repository/Interface/IUserRepository.cs
@@ -2,9 +2,10 @@
 
 namespace ODT_System.Repository.Interface
 {
-    public interface IUserRepository: IBaseRepository
+    public interface IUserRepository : IBaseRepository<User>
     {
         public User? FindByEmail(string email);
+        public User? FindByEmailIncludeRole(string email);
         public User Create(User user);
         public User Update(User user);
         public void Delete(int userId);
