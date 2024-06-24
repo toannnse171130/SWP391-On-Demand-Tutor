93c1824d19d2bf8ef4b0b3fdcc88795de909c777
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-24 22:08:08 +0700

    update Repository

--- ODT_System/Repository/BaseRepository.cs
+++ ODT_System/Repository/BaseRepository.cs
@@ -1,20 +1,47 @@
-﻿using ODT_System.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using ODT_System.Models;
 using ODT_System.Repository.Interface;
 
 namespace ODT_System.Repository
 {
-    public class BaseRepository: IBaseRepository
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
-        private readonly OdtsystemContext _context;
+        protected readonly OdtsystemContext _context;
+        protected readonly DbSet<T> _dbSet;
 
         public BaseRepository(OdtsystemContext context)
         {
             _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
         }
 
         public void Save()
         {
             _context.SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public T? Find(int id)
+        {
+            return _dbSet.Find(id);
         }
     }
 }
--- ODT_System/Repository/Interface/IBaseRepository.cs
+++ ODT_System/Repository/Interface/IBaseRepository.cs
@@ -1,7 +1,12 @@
 ﻿namespace ODT_System.Repository.Interface
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<T> where T : class
     {
+        public IQueryable<T> GetAll();
+        public void Add(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
+        public T? Find(int id);
         public void Save();
     }
 }
