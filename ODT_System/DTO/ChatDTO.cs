7065a743f48641422730f2ad8034a85a0121ffec
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-06-25 22:34:01 +0700

    Create Chat Box DTO

---
+++ ODT_System/DTO/ChatDTO.cs
@@ -0,0 +1,21 @@
+﻿using ODT_System.Models;
+
+namespace ODT_System.DTO
+{
+    public class ChatDTO
+    {
+        public int Id { get; set; }
+
+        public int From { get; set; }
+
+        public int To { get; set; }
+
+        public string Content { get; set; } = null!;
+
+        public DateTime Time { get; set; }
+
+        public virtual UserChatDTO FromNavigation { get; set; } = null!;
+
+        public virtual UserChatDTO ToNavigation { get; set; } = null!;
+    }
+}
---
+++ ODT_System/DTO/ChatInBoxDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ODT_System.DTO
+{
+    public class ChatInBoxDTO
+    {
+
+        public int To { get; set; }
+
+        public string Content { get; set; } = null!;
+    }
+}
