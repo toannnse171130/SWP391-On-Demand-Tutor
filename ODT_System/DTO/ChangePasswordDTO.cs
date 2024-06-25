f183b0aaced04e3275d85aca43b217bd6b15b1a2
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-06-25 22:24:03 +0700

    Create change password

---
+++ ODT_System/DTO/ChangePasswordDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ODT_System.DTO
+{
+    public class ChangePasswordDTO
+    {
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu cũ")]
+        public string OldPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu mới")]
+        public string NewPassword { get; set; } = null!;
+    }
+}
