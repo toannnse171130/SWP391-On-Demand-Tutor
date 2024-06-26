7edfb2cfdf23ffdcefa84b8118dc2a05e1a7d4e1
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-06-26 15:19:58 +0700

    create UseradminDTO

---
+++ ODT_System/DTO/UserAdminDTO.cs
@@ -0,0 +1,25 @@
+﻿using ODT_System.Models;
+
+namespace ODT_System.DTO
+{
+    public class UserAdminDTO
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string FullName { get; set; } = null!;
+
+        public bool Gender { get; set; }
+
+        public DateOnly Dob { get; set; }
+
+        public string Phone { get; set; } = null!;
+
+        public int RoleId { get; set; }
+
+        public string? Desciption { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
