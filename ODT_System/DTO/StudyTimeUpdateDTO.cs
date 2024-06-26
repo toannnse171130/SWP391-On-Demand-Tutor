7ee5a74b852b23e56ccf8f13b1b75038ff0400ef
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-26 15:16:20 +0700

    update StudyTime

---
+++ ODT_System/DTO/StudyTimeUpdateDTO.cs
@@ -0,0 +1,20 @@
+﻿using ODT_System.Enums;
+using ODT_System.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ODT_System.DTO
+{
+    public class StudyTimeUpdateDTO
+    {
+        [Required(ErrorMessage = "Vui lòng nhập thời gian bắt đầu")]
+        [TimeBefore("To", ErrorMessage = "Thời gian bắt đầu trước thời gian kết thúc")]
+        public TimeOnly? From { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập thời gian kết thúc")]
+        public TimeOnly? To { get; set; }
+
+        [EnumValue(typeof(DayOfWeekEnum))]
+        [Required(ErrorMessage = "Vui lòng nhập ngày trong tuần")]
+        public string? DayOfWeek { get; set; }
+    }
+}
