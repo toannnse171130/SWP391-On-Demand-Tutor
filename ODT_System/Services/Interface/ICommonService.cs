19319762a1fc1b15d0dd4d13ce8aeb9ce910b362
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-06-27 21:32:50 +0700

    create common service interface

---
+++ ODT_System/Services/Interface/ICommonService.cs
@@ -0,0 +1,11 @@
+﻿using ODT_System.DTO;
+using ODT_System.SharedObject;
+
+namespace ODT_System.Services.Interface
+{
+    public interface ICommonService
+    {
+        public PostCommonDTO? GetPostById(int id);
+        public PaginatedModel<PostCommonDTO> GetPosts(int? pageIndex, int? pageSize, string? textSearch);
+    }
+}
