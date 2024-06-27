ace342985f2ce5e790dfe516405fa80167253e0b
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-06-27 21:28:27 +0700

    create chathub

---
+++ ODT_System/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ODT_System.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinGroup(string userId)
+        {
+            // Tham gia vào group
+            await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+        }
+
+        public async Task SendMessageToGroup(string userId, string message)
+        {
+            // Gửi tin nhắn chỉ cho các thành viên trong group
+            await Clients.Group(userId).SendAsync("ReceiveMessage", message);
+        }
+    }
+}
