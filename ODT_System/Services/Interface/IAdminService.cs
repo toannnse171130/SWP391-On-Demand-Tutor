3a91eb30ca3f4134002227b52200a7fa28278e0b
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-28 21:57:03 +0700

    Create Admin Service

---
+++ ODT_System/Services/Interface/IAdminService.cs
@@ -0,0 +1,15 @@
+﻿using ODT_System.DTO;
+using ODT_System.SharedObject;
+
+namespace ODT_System.Services.Interface
+{
+    public interface IAdminService
+    {
+        public bool AppovePost(PostApproveDTO postApproveDTO, out string message);
+        public PaginatedModel<PostAdminDTO> GetAllPost(int? pageIndex, int? pageSize, string? status, string? textSearch);
+        public PaginatedModel<UserAdminDTO> GetAllUser(int? pageIndex, int? pageSize, bool? status, string? textSearch);
+        public PostAdminDTO? GetPostDetails(int id);
+        public UserAdminDTO GetUserDetails(int id);
+        public bool UpdateUserStatus(UserUpdateAdminDTO userUpdateAdminDTO, out string message);
+    }
+}
