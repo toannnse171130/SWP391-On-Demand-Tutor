7eaf96da1d583630a58ea673158edd042d40a5e5
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-28 21:58:23 +0700

    Create Share Object

---
+++ ODT_System/SharedObject/Constants.cs
@@ -0,0 +1,10 @@
+﻿using ODT_System.Enums;
+using System;
+
+namespace ODT_System.SharedObject
+{
+    public static class Constants
+    {
+        public const int NumberOfTypeOfFee = 5;
+    }
+}
---
+++ ODT_System/SharedObject/PaginatedModel.cs
@@ -0,0 +1,28 @@
+﻿namespace ODT_System.SharedObject
+{
+    public class PaginatedModel<T>
+    {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+        public int totalItems { get; set; }
+        public int totalPages
+        {
+            get
+            {
+                return (int)Math.Ceiling((double)totalItems / pageSize);
+            }
+        }
+        public object? items { get; set; }
+
+        public static PaginatedModel<T> GetPaging(int? pageIndex, int? pageSize, IQueryable<T> items)
+        {
+            return new PaginatedModel<T>()
+            {
+                pageIndex = pageIndex ?? 1,
+                pageSize = pageSize ?? 10,
+                totalItems = items.Count(),
+                items = items.Skip(((pageIndex ?? 1) - 1) * (pageSize ?? 10)).Take(pageSize ?? 10).ToList()
+            };
+        }
+    }
+}
