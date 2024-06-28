a212520af5af4fe82b0dca1f238039aed37b38a5
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-06-28 21:56:30 +0700

    update Service

--- ODT_System/Services/Interface/IAccountService.cs
+++ ODT_System/Services/Interface/IAccountService.cs
@@ -1,15 +1,23 @@
 ﻿using ODT_System.DTO;
 using ODT_System.Models;
+using ODT_System.SharedObject;
 
 namespace ODT_System.Services.Interface
 {
     public interface IAccountService
     {
-        public User? FindByEmail(string email);
+        public bool ChangePassword(ChangePasswordDTO changePasswordDTO, string email, out string message);
+        public bool CreatePost(PostCreateDTO postCreateDTO, string userEmail, out string message);
+        public ViewProfileDTO? FindUserProfile(string email);
+        public PaginatedModel<PostTutorDTO> ListPost(string userEmail, int? pageIndex, int? pageSize, string? status, string? textSearch);
         public bool NewPassword(NewPasswordDTO newPasswordDTO, out string message);
-        public bool SendOTP(SendOTPDTO sendOTPDTO, out string message);
+        public PostTutorDTO? GetPostById(int id, string userEmail);
         public bool UpdateProfile(UpdateProfileDTO updateProfileDTO, string emailAccount);
         public bool VerifyEmail(VerifyEmailDTO verifyEmailDTO, out string message);
-        public bool VerifyOTP(VerifyOTPDTO verifyOTPDTO, out string message);
+        public bool UpdatePost(PostUpdateDTO postUpdateDTO, string userEmail, out string message);
+        public bool DeletePost(int id, string userEmail, out string message);
+        public List<ChatDTO> ListInbox(string userEmail, int withUser);
+        public List<UserChatDTO> ListChat(string userEmail);
+        public bool TryInbox(ChatInBoxDTO chatInBoxDTO, string userEmail, out string message);
     }
 }
--- ODT_System/Services/Interface/IAuthenticationService.cs
+++ ODT_System/Services/Interface/IAuthenticationService.cs
@@ -5,7 +5,7 @@ namespace ODT_System.Services.Interface
 {
     public interface IAuthenticationService
     {
-        public bool Login(UserLoginDTO user, out string token);
+        public bool Login(UserLoginDTO user, out Dictionary<string, object> data, out string message);
         public bool Register(UserRegisterDTO user, out string message);
     }
 }
