f2fa546df3680f989e43288cd563a72b62e52038
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-06-28 22:05:32 +0700

    create Stringhelper

---
+++ ODT_System/Helpers/StringHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace ODT_System.Helpers
+{
+    public static class StringHelper
+    {
+        public static bool CompareIgnoreCase(string str1, string str2)
+        {
+            return str1.Contains(str2, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
