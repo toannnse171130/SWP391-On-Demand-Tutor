547fb6247b0863c354c4f443751ef2ff1b1027f8
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-07-08 17:38:05 +0700

    create study time dto

---
+++ ODT_System/DTO/StudyTimeDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace ODT_System.DTO
+{
+    public class StudyTimeDTO
+    {
+        public int Id { get; set; }
+
+        public int PostId { get; set; }
+
+        public TimeOnly? From { get; set; }
+
+        public TimeOnly? To { get; set; }
+
+        public string? DayOfWeek { get; set; }
+    }
+}
