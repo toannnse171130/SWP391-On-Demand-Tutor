42eb60a64caac17fe9335e67a9eddb463bd216b8
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-07-08 17:49:40 +0700

    update icommon service

--- ODT_System/Services/Interface/ICommonService.cs
+++ ODT_System/Services/Interface/ICommonService.cs
@@ -7,5 +7,6 @@ namespace ODT_System.Services.Interface
     {
         public PostCommonDTO? GetPostById(int id);
         public PaginatedModel<PostCommonDTO> GetPosts(int? pageIndex, int? pageSize, string? textSearch);
+        public PaginatedModel<UserCommonDTO> GetTutors(int? pageIndex, int? pageSize, string? textSearch);
     }
 }
