325cae4534b3f06670e40071a7c328a8ec1102c5
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-07-08 17:40:14 +0700

    create user admin update

---
+++ ODT_System/DTO/UserUpdateAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ODT_System.DTO
+{
+    public class UserUpdateAdminDTO
+    {
+        [Required(ErrorMessage = "Vui lòng nhập Id của người dùng")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Vui chọn trạng thái")]
+        public bool IsActive { get; set; }
+    }
+}
