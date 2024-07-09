806b634c47c8d36edc4bff63f0f8234ac770e405
dungntse180117 <dungntse180117@fpt.edu.vn>
2024-07-09 07:30:58 +0700

    create viewprofiledto

---
+++ ODT_System/DTO/ViewProfileDTO.cs
@@ -0,0 +1,21 @@
+﻿using ODT_System.Models;
+
+namespace ODT_System.DTO
+{
+    public class ViewProfileDTO
+    {
+        public string Email { get; set; } = null!;
+
+        public string FullName { get; set; } = null!;
+
+        public bool Gender { get; set; }
+
+        public DateOnly Dob { get; set; }
+
+        public string Phone { get; set; } = null!;
+
+        public int RoleId { get; set; }
+
+        public string? Desciption { get; set; }
+    }
+}
