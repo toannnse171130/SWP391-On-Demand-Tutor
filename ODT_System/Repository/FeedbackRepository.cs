c3c619d87786e51ee4ee2d9458dde8d5ba3c8072
toannnse171130 <toannnse171130@fpt.edu.vn>
2024-07-13 10:34:24 +0700

    create Feedback Repository

---
+++ ODT_System/Repository/FeedbackRepository.cs
@@ -0,0 +1,12 @@
+﻿using ODT_System.Models;
+using ODT_System.Repository.Interface;
+
+namespace ODT_System.Repository
+{
+    public class FeedbackRepository : BaseRepository<Feedback>, IFeedbackRepository
+    {
+        public FeedbackRepository(OdtsystemContext context) : base(context)
+        {
+        }
+    }
+}
